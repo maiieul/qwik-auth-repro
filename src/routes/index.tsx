292b118828ab295cfa80846e1806759cbb2dbd9d
maiieul <maieul.chevalier@gmail.com>
2024-02-02 19:12:49 +0100

    add discord auth

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,72 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import {
+  useAuthSignin,
+  useAuthSession,
+  useAuthSignout,
+} from "~/routes/plugin@auth";
 
 export default component$(() => {
+  const signIn = useAuthSignin();
+  const session = useAuthSession();
+  const signOut = useAuthSignout();
+
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
+    <header>
+      <div>
+        <div>
+          <a href="/" title="qwik"></a>
+        </div>
+        <ul>
+          <li>
+            <a
+              href="https://qwik.builder.io/docs/components/overview/"
+              target="_blank"
+            >
+              Docs
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://qwik.builder.io/examples/introduction/hello-world/"
+              target="_blank"
+            >
+              Examples
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://qwik.builder.io/tutorial/welcome/overview/"
+              target="_blank"
+            >
+              Tutorials
+            </a>
+          </li>
+          <li>
+            <span>{session.value?.user?.email}</span>
+          </li>
+          <li>
+            {session.value?.user === undefined && (
+              <button
+                onClick$={() =>
+                  signIn.submit({
+                    providerId: "github",
+                    options: { callbackUrl: "http://localhost:5173/" },
+                  })
+                }
+              >
+                Sign In
+              </button>
+            )}
+          </li>
+          <li>
+            {session.value?.user !== undefined && (
+              <button onClick$={() => signOut.submit({ callbackUrl: "/" })}>
+                Sign Out
+              </button>
+            )}
+          </li>
+        </ul>
+      </div>
+    </header>
   );
 });
-
-export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
-};
